f056cb3b708c46f7715606bce6911864c3b7bb12
gliang <garyliang.cpp@gmail.com>
2022-04-04 22:36:56 -0700

    feat: displayed Search Bar
    Added css style for search bar as placeholder

--- trending-memes/src/App.js
+++ trending-memes/src/App.js
@@ -1,3 +1,4 @@
+import MenuBar from './Components/MenuBar';
 import './Components/Title'
 import Title from './Components/Title';
 
@@ -5,6 +6,8 @@ export default function App() {
   return (
     <div className="App">
       <Title /*name='The Trending Memes'*//>
+
+      <MenuBar/>
     </div>
   );
 }
--- trending-memes/src/Components/MenuBar.js
+++ trending-memes/src/Components/MenuBar.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types'
 
 function MenuBar() {
   return (
-    <div>
-      
+    <div style={divStyle}>
+      <div>Search Bar</div>
     </div>
   )
+}
+
+
+const divStyle = {
+        color: 'blue',
+        padding: '6px 6px 6px 6px',
+        align: 'center',
+        position: 'center',
+        border: '1px solid',
 }
 
 
