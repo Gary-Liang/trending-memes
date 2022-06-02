ad1770460a5fe1d360b098958ec73aefb80b02f9
gliang <garyliang.cpp@gmail.com>
2022-06-01 23:40:45 -0700

    fix: Grabbed env variables from load_dotenv function and .env file
    Grabbed env variables from load_dotenv function and .env file and added auth header

--- trending-memes/src/Backend/API/apiHandler.py
+++ trending-memes/src/Backend/API/apiHandler.py
@@ -1,22 +1,28 @@
-from flask import Flask, json, request, make_response
+from flask import Flask, json, make_response
 import requests
+from requests_oauthlib import OAuth2Session
 from logging.config import dictConfig
 import os
 from dotenv import load_dotenv
 
+# Load variables from .env file (not in version control)
 load_dotenv()
+
 # client id and client secret
-client_id = 'af53be228b9a39e'
-client_secret = os.getenv('CLIENT_SECRET')
+CLIENT_ID = 'af53be228b9a39e'
+CLIENT_SECRET = os.getenv('SECRET_KEY')
+headers = {'Accept':'*/*',
+           'Authorization' : 'Client-ID ' + CLIENT_ID }
 
 # variables to use for API call
-tag_name = ''
-sort_filter = ''
-window_filter = ''
-page_filter = ''
+tag_name = 'funny'
+sort_filter = 'top'
+window_filter = 'week'
+page_filter = '0'
 
 
 # Basic configuration for app for logging
+# Referenced from https://flask.palletsprojects.com/en/2.1.x/logging/
 dictConfig({
     'version': 1,
     'formatters': {'default': {
@@ -41,7 +47,7 @@ def members():
     # Get current directory and output path into terminal console 
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     app.logger.info(SITE_ROOT)
-    app.logger.info(client_secret)
+    app.logger.info(CLIENT_SECRET)
     json_url = os.path.join(SITE_ROOT, "static/data", "MOCK_DATA.json")
     # Load json from a file 
     json_data = json.load(open(json_url))
@@ -58,12 +64,13 @@ def members_two():
     # returns the json data, serialized 
     return json.dumps(json_data)
 
-# Make api calls to imgur gallery tag name calls.   
+# Make API calls to imgur gallery tag name calls.   
 # https://api.imgur.com/3/gallery/t/{{tagName}}/{{sort}}/{{window}}/{{page}}
+# Imgur API uses OAuth 2.0.  get request is different from the norm due to different authentication
 @app.route('/response')
 def response():
     get_request = 'https://api.imgur.com/3/gallery/t/' + tag_name +  '/' + sort_filter + '/' + window_filter + '/' + page_filter
-    r = requests.get(get_request)
+    r = requests.get(get_request, headers=headers, auth=(CLIENT_ID, CLIENT_SECRET))
     return r.text
 
 if __name__ == '__main__':
