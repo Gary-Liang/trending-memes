1389fcc214677973da53f6c265aa3ad960b28e43
Gary Liang <r4dsgary@yahoo.com>
2023-02-10 21:04:59 -0800

    fix: fixed so that preview media has unique id always
    - fixed so that preview media has unique id always
    - loading icon working properly now

--- trending-memes/src/Components/LoadingIcon.js
+++ trending-memes/src/Components/LoadingIcon.js
@@ -1,10 +1,23 @@
 
 export default function LoadingIcon() {
 
+    const loadingstyle = {
+        background: 'rgba(0,0,0, 0.75)',
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        zIndex: "5",
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white"
+    }
+
 
     return(
         <>
-            <div className='loadingIcon'>
+            <div className='loadingIcon' style={loadingstyle}>
                 Loading . . . 
             </div>
         </>
--- trending-memes/src/Components/SearchResults.js
+++ trending-memes/src/Components/SearchResults.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export default function SearchResults({query, setMediaInfo, setAlbumInfo}) {
+export default function SearchResults({query, setMediaInfo, setAlbumInfo, setLoading}) {
 
   // create state variable to get backend API 
  const [data, setData] = useState([{}]);
@@ -10,16 +10,19 @@ export default function SearchResults({query, setMediaInfo, setAlbumInfo}) {
 
  // Purpose of useEffect is to define some anonymous lambda function inside the parameters to use it after 
  useEffect(() => {
+    setLoading(true);
     fetch(`/search?q=${query}`).then(
       // Promise
       res => res.json()
     ).then(
       data => {
-        setData(JSON.parse(JSON.stringify(data)).data.items)
-        console.log(JSON.parse(JSON.stringify(data)).data.items)
+        setData(JSON.parse(JSON.stringify(data)).data.items);
+        //console.log(JSON.parse(JSON.stringify(data)).data.items);
+        console.log(JSON.parse(JSON.stringify(data)));
+        setLoading(false);
       }
     ) 
-  }, [query])  // by putting query as a dependency here, we render more than once, every time the query changes.
+  }, [query, setLoading])  // by putting query as a dependency here, we render more than once, every time the query changes.
 
   const divStyle = {
     color: 'black',
@@ -65,10 +68,10 @@ const mediaMaxSize = {
 // it is called for imgur
 function renderMediaPreview(data) {
   console.log('Rendering.');
-  if (data.link) {
+  if (data && data.link) {
     if (data.link.includes(".mp4")) {
       return (
-        <video key={data.link} style={mediaMaxSize} preload="auto" controls autoPlay muted loop>
+        <video key={data.id} style={mediaMaxSize} preload="auto" controls autoPlay muted loop>
           <source src={data.link} type="video/mp4"/>
         </video>
       )
@@ -81,20 +84,20 @@ function renderMediaPreview(data) {
       if (data.images[0].type.includes("mp4")) {
         //mediaURL += "mp4";
         return (
-          <>
-          <p>{previewHeaderText}</p>
-          <video key={mediaURL} style={mediaMaxSize} preload="auto" controls autoPlay muted loop>
-            <source src={mediaURL} type="video/mp4"/>
-          </video>
-          </>
+          <div key={data.id}>
+            <p>{previewHeaderText}</p>
+            <video key={mediaURL} style={mediaMaxSize} preload="auto" controls autoPlay muted loop>
+              <source src={mediaURL} type="video/mp4"/>
+            </video>
+            </div>
         )
       } else {
         //mediaURL += data.images[0].type.split("/")[1];
         return (
-          <>
-          <p>{previewHeaderText}</p>
-          <img key={mediaURL} src={mediaURL} style={mediaMaxSize} alt=""/>
-          </>
+          <div key={data.id}>
+            <p>{previewHeaderText}</p>
+            <img key={mediaURL} src={mediaURL} style={mediaMaxSize} alt=""/>
+          </div>
         )
       }
     } else {
@@ -172,12 +175,16 @@ function writeMetadataToMediaInfo(data, setMediaInfo, setAlbumInfo) {
               } else if (data.title.toLowerCase().includes(query.toLowerCase())) { 
                   // if condition is true, then return data that matches query to be mapped
                   return data;
+              } else {
+                return {};
               }
           }).map((data) => (
-              <div key={data.id} className={data.id} style={searchResults} onClick={() => writeMetadataToMediaInfo(data, setMediaInfo, setAlbumInfo)}>
+              // data can be an empty list {}
+              Object.keys(data).length !== 0 ? 
+                <div key={data.id} className={data.id} style={searchResults} onClick={() => writeMetadataToMediaInfo(data, setMediaInfo, setAlbumInfo)}>
                   <p>{data.title}</p>
                   {renderMediaPreview(data)}
-              </div>
+                </div> : null
 
           ))
         }
@@ -185,112 +192,3 @@ function writeMetadataToMediaInfo(data, setMediaInfo, setAlbumInfo) {
     </>
   )
 }
-
-
-// const divStyle = {
-//     color: 'black',
-//     padding: '0.25%',
-//     border: '3px solid grey',
-//     textAlign: 'center',
-//     // display: 'flex',
-//     display: 'grid',
-//     // flexDirection: 'row',
-//     // flexWrap: 'wrap',
-//     // Condenses flex direction and flex wrap
-//     // flexFlow: 'row wrap',
-//     // alignItems: 'space-around',
-//     // justifyContent: 'space-around',
-//     // alignContent: 'space-between',
-//     gridTemplateColumns: 'repeat(4, 0.25fr)',
-//     gridAutoFlow: 'row',
-//     //minWidth: '100px',
-//     gap: '6.5px',
-//     //gridRowStart: 'span 2',
-
-// }
-
-// const searchResults = {
-//     border: 'black ridge 1px',
-//     borderRadius: '10px',
-//     alignSelf: 'flex-start',
-//     position: 'relative',
-//     // margin: 'auto',
-//     // width: '30%',
-//     padding: '25px',
-    
-// }
-
-// const mediaMaxSize = {
-//   maxHeight: '450px',
-//   maxWidth: '250px',
-//   height: 'auto',
-//   width: 'auto',
-// }
-
-// // functions should be declared outside of the functional components or else we re-render the function every time 
-// // it is called for imgur
-// function renderMediaPreview(data) {
-//   console.log('Rendering.');
-//   if (data.link) {
-//     if (data.link.includes(".mp4")) {
-//       return (
-//         <video key={data.link} style={mediaMaxSize} preload="auto" controls autoPlay muted loop>
-//           <source src={data.link} type="video/mp4"/>
-//         </video>
-//       )
-//     } else if (data.link.includes("/a/")) {
-//       let mediaURL = getMediaLink(data);
-//       //let mediaURL = "http://i.imgur.com/" + data.cover + ".";
-//       let previewHeaderText = "";
-//       if (data.images_count > 1)
-//         previewHeaderText = "(Meme Album)";
-//       if (data.images[0].type.includes("mp4")) {
-//         //mediaURL += "mp4";
-//         return (
-//           <>
-//           <p>{previewHeaderText}</p>
-//           <video key={mediaURL} style={mediaMaxSize} preload="auto" controls autoPlay muted loop>
-//             <source src={mediaURL} type="video/mp4"/>
-//           </video>
-//           </>
-//         )
-//       } else {
-//         //mediaURL += data.images[0].type.split("/")[1];
-//         return (
-//           <>
-//           <p>{previewHeaderText}</p>
-//           <img key={mediaURL} src={mediaURL} style={mediaMaxSize} alt=""/>
-//           </>
-//         )
-//       }
-//     } else {
-//       // if link is just a normal image or a gifv, render it normally. 
-//       return (
-//         <img key={data.link} src={data.link} style={mediaMaxSize} alt=""/>
-//       )
-//     }
-//   }
-// }
-
-// // getter function - generate a media link based on data passed in and what type of media it is 
-// // based off of imgur's url
-// function getMediaLink(data) {
-//   if (data.link) {
-//     if (data.link.includes("/a/")) {
-//       return data.images[0].link;
-//     } else {
-//       return data.link;
-//     }
-//   }
-// }
-
-// // data.id is album hash link
-// function writeMetadataToMediaInfo(data, setMediaInfo, setAlbumInfo) {
-//   if (data.images_count > 1) {
-//     setMediaInfo({dataInfo: data, isClicked: true, mediaLink: getMediaLink(data), height: data.height, width: data.width});
-//     setAlbumInfo({album: data.id, albumLength: data.images_count});
-//   } else {
-//     setMediaInfo({dataInfo: data, isClicked: true, mediaLink: getMediaLink(data), height: data.height, width: data.width});
-//     setAlbumInfo({album: null, albumLength: 1});
-//   }
-// }
