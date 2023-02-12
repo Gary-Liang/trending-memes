3be2d0ef4c6fd0435144303f21480fbc38509abe
gliang <garyliang.cpp@gmail.com>
2023-02-11 22:56:45 -0800

    fix: did npm updates and package updates
    did npm updates and package updates

--- trending-memes/src/index.js
+++ trending-memes/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +9,15 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
 
 
 // If you want to start measuring performance in your app, pass a function
