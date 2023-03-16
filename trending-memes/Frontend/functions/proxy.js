5fc03426a51b7670bd3939ec73ff09720a81b21a
gliang <garyliang.cpp@gmail.com>
2023-03-15 22:25:07 -0700

    fix: changed .conf file to a netlify function
    - changed .conf file to a netlify function

---
+++ trending-memes/Frontend/functions/proxy.js
@@ -0,0 +1,25 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const options = {
+    target: 'https://trending-memes-production.up.railway.app',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/search:' : '/search'
+    }
+
+};
+
+const proxy = createProxyMiddleware(options);
+
+exports.handler = async (event, context) => {
+    return new Promise((resolve, reject) => {
+        proxy(event, context, (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
