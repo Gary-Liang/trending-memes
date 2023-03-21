168fa98d71cc9ebe59f48f0d22fde7207a3b8feb
gliang <garyliang.cpp@gmail.com>
2023-03-20 22:30:48 -0700

    feat: added additional functions for netfliy to use
    - added additional functions for netfliy to use

---
+++ trending-memes/Frontend/functions/search.js
@@ -0,0 +1,34 @@
+
+const API_ENDPOINT = 'https://trending-memes-production.up.railway.app/search?q=';
+//const API_ENDPOINT = `${RAILWAY_API_ENDPOINT}/search`;
+
+
+exports.handler = async (event, context) => {
+  try {
+    const query = event.queryStringParameters.q;
+    const response = await fetch(API_ENDPOINT + `${query}`);
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://tmback.xyz',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive'
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://tmback.xyz',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive'
+      },
+      body: JSON.stringify({
+        error: 'Internal Server Error',
+      }),
+    };
+  }
+};
