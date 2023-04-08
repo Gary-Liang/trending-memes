200154e77b34aae9b5e157ff7c1fae195b219463
Gary Liang <r4dsgary@yahoo.com>
2023-04-07 21:31:18 -0700

    feat: added overlay to login screen
    - added overlay to login screen

--- trending-memes/Frontend/src/Components/NavBar.js
+++ trending-memes/Frontend/src/Components/NavBar.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-export default function MenuBar({setShowRegistrationModal, setShowLoginModal}) {
+export default function NavBar({setShowRegistrationModal, setShowLoginModal}) {
+
 
   const menuBarStyle = {
     listStyle: 'none',
     display: 'flex',
     justifyContent: 'center', // We can use justify content after display property
-    gap: '3%'
+    gap: '3%',
+    padding: '15px'
   }
 
   return (
     <nav className='menuBar' style={menuBarStyle}>
-      <li className='signUpLink'>Sign Up</li>
-      <li className='logInLink'>Log In</li>
+      <li className='signUpLink' onClick={() => setShowRegistrationModal(true)}>Sign Up</li>
+      <li className='logInLink' onClick={() => setShowLoginModal(true)}>Log In</li>
       <li className='aboutLink'>About</li>
     </nav>
   )
